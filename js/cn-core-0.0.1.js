ecced8433f78f7472395b46281cba955b9353dc8
hwding <m@amastigote.com>
2017-06-04 23:16:19 +0800

    minor bug fixes

--- js/cn-core-0.0.1.js
+++ js/cn-core-0.0.1.js
@@ -1,29 +1,29 @@
 var create = function (item, server_url, success_method) {
-    general_query(item, server_url, "POST", success_method);
+    general_query(JSON.stringify(item), server_url, "POST", success_method);
 };
 
 var update = function (item, server_url, success_method) {
-    general_query(item, server_url, "PUT", success_method);
+    general_query(JSON.stringify(item), server_url, "PUT", success_method);
 };
 
 var remove = function (item, server_url, success_method) {
-    general_query(item, server_url, "DELETE", success_method);
+    general_query(JSON.stringify(item), server_url, "DELETE", success_method);
 };
 
 var get_item = function (item, server_url, success_method) {
     general_query(item, server_url, "GET", success_method);
 };
 
-var general_query = function (item, server_url, query_method, success_method) {
+var general_query = function (item, server_url, query_method, success_callback) {
     $.ajax({
         type: query_method,
         url: "https://localhost:8443/item",
         data: item,
+        contentType: "application/json",
         dataType: "json",
-        success: success_method,
+        success: success_callback,
         error: function (request, status) {
             // todo
-            change_buttons_clickability(true);
         }
     });
 };
@@ -34,9 +34,4 @@ var get_tags = function (server_url) {
 
 var check_connection = function (server_url) {
 
-};
-
-var translate_action_result = function (error_result) {
-    console.log(error_result);
-    console.log(action_code[error_result.code]);
 };
--- js/cn-panel-0.0.1.js
+++ js/cn-panel-0.0.1.js
@@ -7,10 +7,24 @@ var tag_input = $('#input_tag');
 
 add_btn.click(function () {
     add_btn.prop('disabled', true);
-    create(collect_item(), "https://localhost:8443/item", function (result) {
-        translate_action_result(result);
+    create(collect_item(), "", function (result) {
         update_btn.prop('disabled', false);
         delete_btn.prop('disabled', false);
+    });
+});
+
+update_btn.click(function () {
+    update_btn.prop('disabled', true);
+    update(collect_item(), "", function (result) {
+        update_btn.prop('disabled', false);
+    });
+});
+
+delete_btn.click(function () {
+    delete_btn.prop('disabled', true);
+    remove(collect_item(), "", function (result) {
+        add_btn.prop('disabled', false);
+        update_btn.prop('disabled', true);
     });
 });
 
@@ -18,16 +32,12 @@ var collect_item = function () {
     return {
         name: escape(title_input.val()),
         url: url_input.val(),
-        /*
-         todo tag cannot be empty
-         */
         tag: tag_input.val().split(",").map(function (e) { return escape(e.trim()); }),
         auto_add_tag: true
     };
 }
 
-browser.tabs
-    .query({ currentWindow: true, active: true })
+browser.tabs.query({ currentWindow: true, active: true })
     .then(function (tabs) {
         if (tabs[0]) {
             title_input.val(tabs[0].title);
