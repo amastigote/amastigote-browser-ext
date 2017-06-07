fce2774e69e18e0d52b6eb759bf75931cf044230
hwding <m@amastigote.com>
2017-06-07 23:17:54 +0800

    add action-code

--- js/cn-action-code-0.0.1.js
+++ js/cn-action-code-0.0.1.js
@@ -4,11 +4,13 @@ var action_code = {
     701: "item saved",
     702: "item updated",
     703: "item deleted",
+    704: "items listed",
 
     750: "tags up-to-date",
     751: "tags out-of-date",
 
     // error codes
     800: "item not found",
-    801: "item already exists"
+    801: "item already exists",
+    802: "no items listed"
 };
