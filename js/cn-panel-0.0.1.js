bed7164612af420864f3c465ea0d65aa79d9bfde
hwding <m@amastigote.com>
2017-06-08 22:54:33 +0800

    fix a bug in Chinese comma replacing

--- js/cn-panel-0.0.1.js
+++ js/cn-panel-0.0.1.js
@@ -54,7 +54,7 @@ var collect_item = function () {
     return {
         name: escape(title_input.val()),
         url: url_input.val(),
-        tag: tag_input.val().replace("，", ",").split(",").map(function (e) { return escape(e.trim()); }),
+        tag: tag_input.val().replace(new RegExp("，", 'g'), ",").split(",").map(function (e) { return escape(e.trim()); }),
         auto_add_tag: true
     };
 }
