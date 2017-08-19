f2ca15b5be48fb1083933d6091462236be14b579
hwding <m@amastigote.com>
2017-08-19 10:41:21 +0800

    update package tool

--- pack.sh
+++ pack.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#@author: hwding
-#@date: AUG/19/2017
-#@description: package a minified extension for ama-open
+#AUTH hwding
+#DATE AUG/19/2017
+#DESC package a minified extension for ama-open
 
 zip -r installable-ama-ext.zip . -x art/\* -x coffee/\* -x README.md -x pack.sh
