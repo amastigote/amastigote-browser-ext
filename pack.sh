68a1599c3f3b3666e3f32c01fde63b669a03bed5
hwding <m@amastigote.com>
2017-08-21 01:26:12 +0800

    - update pack tool - fix typo

--- pack.sh
+++ pack.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 #AUTH hwding
-#DATE AUG/19/2017
-#DESC package a minified extension for ama-open
+#DATE AUG/21/2017
+#DESC package a minimized extension for ama-open
 
-zip -r installable-ama-ext.zip . -x art/\* -x coffee/\* -x README.md -x pack.sh -x .git/\*
+zip -r installable-ama-ext.zip . \
+    -x art/\* \
+    -x coffee/\* \
+    -x README.md \
+    -x pack.sh \
+    -x .git/\*
