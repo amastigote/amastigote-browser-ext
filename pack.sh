853311c4a5761c97c5669599796e1463cc27cee2
hwding <m@amastigote.com>
2017-08-21 01:01:50 +0800

    update pack tool

--- pack.sh
+++ pack.sh
@@ -4,4 +4,4 @@
 #DATE AUG/19/2017
 #DESC package a minified extension for ama-open
 
-zip -r installable-ama-ext.zip . -x art/\* -x coffee/\* -x README.md -x pack.sh
+zip -r installable-ama-ext.zip . -x art/\* -x coffee/\* -x README.md -x pack.sh -x .git/\*
